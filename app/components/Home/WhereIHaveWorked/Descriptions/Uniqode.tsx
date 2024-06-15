97e2e70e8af9c5bb6db1f179e7dffe69d648656f
umeshkaushik21 <kaushikumesh21@gmail.com>
2024-06-15 12:06:27 +0530

    fix minor bug

--- app/components/Home/WhereIHaveWorked/Descriptions/Uniqode.tsx
+++ app/components/Home/WhereIHaveWorked/Descriptions/Uniqode.tsx
@@ -2,28 +2,33 @@ import React, { useState } from "react";
 import ArrowIcon from "../../../Icons/ArrowIcon";
 import { getTasksTextWithHighlightedKeyword } from "./taskAndType";
 import { motion } from 'framer-motion';
+type experienceProps={
+  title:string;
+  achivement: TaskProps[]
+}
 type TaskProps = {
   text: string;
   keywords: string[];
 };
+type TasksProps={
+  tasks:experienceProps;
+}
 
-type UniqodeProps = {
-  tasks:TaskProps[];
-};
-export default function Uniqode({tasks}:UniqodeProps) {
-  
+
+export default function Uniqode({tasks}:TasksProps) {
   const [isOn,setIsOn]=useState(false);
-
   const handleClick= ()=>{
     setIsOn(!isOn);
   }
 
+  const achivement=tasks.achivement;
+  const title=tasks.title;
   return (
     <>
       <div className="flex flex-col space-y-5 max-w-xl px-4 md:px-0">
         <div className="flex flex-col spacey-y-2">
           <span className="text-gray-100 sm:text-lg text-sm font-Arimo tracking-wide">
-            Software Engineer <span className="text-red-700">@ Uniqode</span>
+            {title} <span className="text-red-700">@ Uniqode</span>
           </span>
           <span className="font-mono text-xs text-gray-500">Mar 2023 - Present</span>
           <span
@@ -35,7 +40,7 @@ export default function Uniqode({tasks}:UniqodeProps) {
           </span>
         </div>
         <motion.div initial={false} animate={{height: isOn?"auto":"155px"}} transition={{duration:1}} className="overflow-scroll space-y-4 sm:text-sm text-xs">
-          {tasks.map((item, index) => {
+          {achivement.map((item, index) => {
             return (
               <div key={index} className="flex flex-row space-x-1">
                 <ArrowIcon className={" h-5 w-4 text-red-700 flex-none"} />
